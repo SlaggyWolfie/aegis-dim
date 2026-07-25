7a7dd210ec2286fe8fc0a2dcbe7043ada94ee5cb
Slavi Stoev <sl_stoev@abv.bg>
2026-07-26 00:36:40 +0300

    version de-bump

--- dim-aegis.user.js
+++ dim-aegis.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         DIM Aegis Overlay - Silly tests
-// @version      1.4.2
+// @version      1.4.1
 // @namespace    Silly
 // @author       Silly
 // @description  Overlays Aegis weapon tier list data on DIM item popups
